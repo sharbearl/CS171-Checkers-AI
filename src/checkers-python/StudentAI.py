9087c9b3fd75f21a7d5cefeb2dcb5d149009edb5
jtn03 <n.enelyaj03@gmail.com>
2025-11-04 23:47:12 -0800

    Added MCTS structure: select and expand. Also modified Node class: added is_fully_expanded()

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -5,6 +5,7 @@ from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
+EXPLORATION_PARAM = 1 ## Adjust c constant for better results
 class StudentAI():
 
     def __init__(self,col,row,p):
@@ -79,7 +80,7 @@ class Node:
         return self.parent
     
     def get_children(self) -> List[Node]:
-        """ Returns a lsit of nodes representing the children of the current node """
+        """ Returns a list of nodes representing the children of the current node """
         return self.children
     
     def get_total_games(self) -> int:
@@ -90,15 +91,18 @@ class Node:
         """ Return the total times the node has resulted in a winning game """
         return self.wins
     
-    def get_possible_moves(self) -> List[Move]:
-        """ Returns a list of possible moves the node can take """
-        return self.total_games
+    # def get_possible_moves(self) -> List[Move]:
+    #     """ Returns a list of possible moves the node can take """
+    #     return self.total_games
     
     def get_untried(self) -> List[Move]:
         """ Returns a list of moves not yet tried """
-        return self.wins
-    
-    def get_best_child(self, c: int | float) -> Node:
+        return self.untried
+
+    def is_fully_expanded(self)-> bool:
+        return len(self.untried) == 0
+
+    def get_best_child(self, c = EXPLORATION_PARAM) -> Node:
         """ Returns the child with the best UCT value of the current node 
             UCT = wi/si + c * sqrt
         """
@@ -119,12 +123,9 @@ class Node:
         self.children.append(new_node)
         return new_node
 
-    def add_children(self, moves: List[Move]):
-        for move in moves:
-            self.add_child(move)
-
-    def is_win(self):
-        pass
+    # def add_children(self, moves: List[Move]):
+    #     for move in moves:
+    #         self.add_child(move)
 
     def update_node(self, win):
         """ win = 0 if loss
@@ -134,9 +135,41 @@ class Node:
             return
         self.total_games += 1
         self.wins += win
-        self.parent.update_node(win)
+        # self.parent.update_node(win) # parent nodes will update when backpropagating
     
 
-class MCSTree:
-    def __init__(self):
-        self.head = None
+class MCTS:
+    def __init__(self, color, num_simulations):
+        self.color = color
+        self.num_simulations = num_simulations
+    
+    def select(self, node: Node):
+        """ Select node to expand based on UCT """
+        while node.get_children() and node.is_fully_expanded(): # The node has children and every child has been added to compute UCT
+            node = node.get_best_child()
+        return node
+
+    def expand(self, node: Node, color, board: Board):
+        """ Expand children of selected node """
+        untried_moves = node.get_untried()
+        if not untried_moves:   # Check if there are possible moves; if empty, populate the list
+            untried_moves = [move for moves in board.get_all_possible_moves(color) for move in moves]
+        if not node.untried_moves:  # If stil empty after populate, reached terminal node
+            return node
+        move = untried_moves.pop(random.randint(len(untried_moves))) # Pick a random child to expand
+        board.make_move(move, color)
+        child = node.add_child(move)
+        return child
+    
+    def simulate(self, color, board: Board):
+        """ Simulate games from child node """
+    
+    def backpropagate(self, node: Node, win: int, board: Board):
+        """ Backpropagate and update statistics.
+            win = 0 if loss
+            win = 1 if win/tie """
+
+    def search(self, root_board: Board) -> Move:
+        """ Do search for best move to return """
+
+
