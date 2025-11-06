2b58bbe55ab480bc2a5a9df5469f6231d10716de
Sharon Leo <ssleo1@uci.edu>
2025-11-06 11:27:26 -0800

    Made some fixes to algorithm

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -6,6 +6,9 @@ from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 EXPLORATION_PARAM = 1 # C constant for UCT calculations
+### For testing purposes
+# file = open("results.txt", "a")
+###
 class StudentAI():
 
     def __init__(self,col,row,p):
@@ -95,7 +98,7 @@ class Node:
                 score = uct
            
         if not best_child:              # If all children nodes have a value of 0, randomly select a node
-            random.choice(self.children)
+            best_child = random.choice(self.children)
         return best_child
     
     def add_child(self, move: Move, moves: List[Move]) -> "Node":
@@ -160,18 +163,18 @@ class MCTS:
         """
         i = 0                                                   # Tracks how many simulated moves were made
         color = node.get_color()                                # Tracks the color of the current move
-        while self.board.is_win(color) == 0:                    # While no outcome has been determined yet
+        while self.board.is_win(3 - color) == 0:                    # While no outcome has been determined yet
             moves = self.board.get_all_possible_moves(color)    # Get all possible moves according to color player
-            # print(self.board.is_win(color))
+            # print(color, file=file)
             # print(self.board.get_all_possible_moves(color))
             move = random.choice(random.choice(moves))          # Randomly select a move
             self.board.make_move(move, color)                   # Perform move
-            # self.board.show_board()
-            # print("simulated", i)
+            # self.board.show_board(file)
+            # print("simulated", i, file=file)
             color = 3 - color                                   # Swap color, 1 if color was 2, 2 if color was 1
             i += 1                                              # Increase move counter by 1
         result = self.board.is_win(color)                       # -1 for tie, 1 for black win, 2 for white win
-        for j in range(i - 1):                                  # Undo all moves back to child node
+        for _ in range(i - 1):    # double check                              # Undo all moves back to child node
             self.board.undo()
         return 1 if node.get_color() == result or node.get_color() == -1 else 0 # 1 if winner is same as node color
                                                                                 # or if tie, otherwise 0
@@ -184,7 +187,8 @@ class MCTS:
         if node == None:                                  # If node is none, then this is the tree root node
             return
         self.board.undo()                                 # Undo move
-        # self.board.show_board()
+        # print("undoing move", file=file)
+        # self.board.show_board(file)
         node.update_node(reward)                          # Update the values of the current node
         self.backpropagate(node.get_parent(), 1 - reward) # Propagate to previous node, flip reward value
 
