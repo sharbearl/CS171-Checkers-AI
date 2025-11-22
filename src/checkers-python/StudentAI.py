4f517e464476a6fc381dbc68993de4009a4479c4
Sharon Leo <ssleo1@uci.edu>
2025-11-22 13:19:48 -0800

    Cleaned up comments and extraneous code

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -8,7 +8,6 @@ from BoardClasses import Checker
 #Students can modify anything except the class name and exisiting functions and varibles.
 EXPLORATION_PARAM = .5 # C constant for UCT calculations
 
-### Version 3: Tree saving and Board copies instead of undo ###
 class StudentAI():
 
     def __init__(self,col,row,p):
@@ -29,28 +28,30 @@ class StudentAI():
 
         moves = self.board.get_all_possible_moves(self.color)
         
-        if len(moves) == 1 and len(moves[0]) == 1:      # If only one move, just perform that move
+        if len(moves) == 1 and len(moves[0]) == 1:                          # If only one move, just perform that move
             move = moves[0][0]
         else:
-            root = Node(None, self.opponent[self.color], None, moves, self.board)  # Create first node of the tree
-            tree = MCTS()                     # Create tree structure
-            move = tree.search(root)                    # Return the best move to make from the root node
+            root = Node(None, self.opponent[self.color], None, self.board)  # Create first node of the tree
+            tree = MCTS()                                                   # Create tree structure
+            move = tree.search(root)                                        # Return the best move to make from the root node
 
-        self.board.make_move(move,self.color)           # Perform the move
+        self.board.make_move(move,self.color)                               # Perform the move
 
         return move
 
 
 def copy_board(board: Board) -> Board:
-    """Creates a deep copy of a Board object."""
-    new_board = Board(board.col, board.row, board.p)
-    new_board.tie_counter = board.tie_counter
+    """ Creates a deep copy of a Board object.
+        @param: board - The board object to be created
+        @return: a board object copied from the given board
+    """
+    new_board = Board(board.col, board.row, board.p)    # Create new board
+    new_board.tie_counter = board.tie_counter           # Copy over member data
     new_board.tie_max = board.tie_max
     new_board.black_count = board.black_count
     new_board.white_count = board.white_count
-    # Doesn't copy saved_moves since we are not using undo
 
-    for row in range(board.row):
+    for row in range(board.row):    # For each piece, copy the Checker piece at that cell
         for col in range(board.col):
             piece = board.board[row][col]
             new_board.board[row][col] = Checker.Checker(piece.color, [piece.row, piece.col])
@@ -60,20 +61,21 @@ def copy_board(board: Board) -> Board:
 
 
 class Node:
-    def __init__(self, parent: "Node", color: int, move: Move, moves: List[Move], board: Board) -> None:
+    def __init__(self, parent: "Node", color: int, move: Move, board: Board) -> None:
         """ Initialize values of a Node """
-        self.move = move             # The Move which brings you to this node
-        self.parent = parent         # The Node that comes before this node
-        self.children = []           # A list of Nodes representing the children nodes
-        self.total_games = 0         # The number of times the node has been visited
-        self.wins = 0                # The number of times the node results in a winning result
-        # self.color = color           # The color of the player at this node
-        self.board = copy_board(board)
-        self.prev_color = color
-        self.next_color = 3 - color
-        if move != None:
+        self.move = move                # The Move which brings you to this node
+        self.parent = parent            # The Node that comes before this node
+        self.children = []              # A list of Nodes representing the children nodes
+        self.total_games = 0            # The number of times the node has been visited
+        self.wins = 0                   # The number of times the node results in a winning result
+        self.board = copy_board(board)  # A copy of the parent node's board
+        self.prev_color = color         # The color of the player that led to this node
+        self.next_color = 3 - color     # The color of the player that will leave this node
+
+        if move != None:                # If there was a parent node, the board must be updated to the current move
             self.board.make_move(move, color)
-        self.untried_moves = []
+        
+        self.untried_moves = []         
         for possible_moves in self.board.get_all_possible_moves(self.next_color): # Add all moves within inner lists of moves
             self.untried_moves.extend(possible_moves) # A list of Moves that have not been made into a child node
 
@@ -90,10 +92,11 @@ class Node:
         return self.untried_moves
     
     def get_color(self) -> int:
-        """ Returns color of node's turn, 1 if black, 2 if white """
+        """ Returns color of player that led to this node, 1 if black, 2 if white """
         return self.prev_color
     
     def get_next_color(self) -> int:
+        """ Returns color of the next turn, 1 if black, 2 if white """
         return self.next_color
 
     def has_children(self) -> bool:
@@ -130,16 +133,15 @@ class Node:
             best_child = random.choice(self.children)
         return best_child
     
-    def add_child(self, move: Move, moves: List[Move]) -> "Node":
+    def add_child(self, move: Move) -> "Node":
         """ Adds a new child node to the current node and
             removes the corresponding move from untried_moves
             @param:  move - Move representing the move that brings you to this node
-                     moves - List of possible Moves from this node
             @return: Node corresponding to the newly created child node
         """
-        new_node = Node(self, self.next_color, move, moves, self.board) # Flip color, child is opposite color from current node
-        self.children.append(new_node)                     # Add child to children list
-        self.untried_moves.remove(move)                    # Remove move as a possible move
+        new_node = Node(self, self.next_color, move, self.board) # Flip color, child is opposite color from current node
+        self.children.append(new_node)                           # Add child to children list
+        self.untried_moves.remove(move)                          # Remove move as a possible move
         return new_node
 
     def add_children(self, moves: List[Move]) -> None:
@@ -171,33 +173,26 @@ class MCTS:
     def __init__(self, num_simulations=500):
         """ Initialize values of a Monte Carlo Tree Search """
         self.num_simulations = num_simulations  # Number of iterations to run
-        # self.board = board                      # Reference to the game board
     
     def select(self, node: Node) -> Node:
         """ Select node to expand based on UCT 
             @param:  node - Node to start selection process from (root)
             @return: Deepest non-fully-expanded Node
         """
-        # board_copy = copy_board(self.board)
-
         while node.has_children() and node.is_fully_expanded(): # While node is nonterminal and fully expanded
-            color = 3 - node.get_color()
             node = node.get_best_child()                        # Get best child using UCT
-            # board_copy.make_move(node.get_move(), color)        # Perfom move
-        return node #, board_copy
+        return node
 
     def expand(self, node: Node) -> Node:
         """ Expand children of selected node 
             @param:  node - Node selected to expand
             @return: Newly created expanded child Node
         """
-        if node.board.is_win(node.get_color()) != 0:                    # Node is terminal, no expansion possible
+        if node.board.is_win(node.get_color()) != 0:    # Node is terminal, no expansion possible
             return node
         
-        move = random.choice(node.get_untried_moves())                  # Pick a random move to create expanded node
-        # board.make_move(move, 3 - node.get_color())                    # Perform move
-        moves = node.board.get_all_possible_moves(3 - node.get_color()) # Get all possible moves for child node
-        new_node = node.add_child(move, moves)                          # Create child node
+        move = random.choice(node.get_untried_moves())  # Pick a random move to create expanded node
+        new_node = node.add_child(move)                 # Create child node
         return new_node
 
     def simulate(self, node: Node) -> int:
@@ -243,11 +238,11 @@ class MCTS:
             @param:  node - Node representing current state, tree root node
             @return: Move representing best move based on UCT value
         """
-        for _ in range(self.num_simulations):                            # Runs for specified number of iterations
-            leaf = self.select(node)                         # Select a leaf node to perform expansion
-            new_child = self.expand(leaf)         # Create child node from leaf node                   
-            reward = self.simulate(new_child)                 #  Simulate game from the child node and retrieve the outcome                   
-            self.backpropagate(new_child, reward) # Update all previous values based on the reward outcome
+        for _ in range(self.num_simulations):       # Runs for specified number of iterations
+            leaf = self.select(node)                # Select a leaf node to perform expansion
+            new_child = self.expand(leaf)           # Create child node from leaf node                   
+            reward = self.simulate(new_child)       # Simulate game from the child node and retrieve the outcome                   
+            self.backpropagate(new_child, reward)   # Update all previous values based on the reward outcome
 
-        return self.select_move(node)         # Select the best move from the current node
+        return self.select_move(node)               # Select the best move from the current node
 
